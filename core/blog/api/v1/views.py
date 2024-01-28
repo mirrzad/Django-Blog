1dd9f49f8106ed21b157334585bc7d7e985f72e2
mirrzad <azadamir993@gmail.com>
2024-01-29 01:25:40 +0330

    adding cache system with redis

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,11 +2,12 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework.permissions import IsAuthenticated
 from ...models import Blog
 from .serializers import BlogSerializer
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 
 
 class BlogListApi(ListCreateAPIView):
     permission_classes = [IsAuthenticated]
-    # queryset = Blog.objects.filter(is_active=True)
     serializer_class = BlogSerializer
 
     def get_queryset(self, **kwargs):
@@ -16,10 +17,12 @@ class BlogListApi(ListCreateAPIView):
             query = Blog.objects.filter(is_active=True, category__id=self.kwargs.get('pk'))
         return query
 
+    @method_decorator(cache_page(60 * 30))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
 
 class BlogDetailApi(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = BlogSerializer
     queryset = Blog.objects.filter(is_active=True)
-
-
--- core/core/settings.py
+++ core/core/settings.py
@@ -153,3 +153,14 @@ SPECTACULAR_SETTINGS = {
 }
 
 CELERY_BROKER_URL = 'redis://redis:6379/1'
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
